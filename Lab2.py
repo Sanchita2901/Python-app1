323ab05d3c32ac5bc94a2bc502dab5890cbd7756
Sanchita2901 <155062712+Sanchita2901@users.noreply.github.com>
2023-12-28 15:18:10 +0530

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,20 @@
+def div(p,q):
+    output=p/q
+    return (output)
+def division():
+    x=int(input("Enter the 1st number:"))
+    y=int(input("Enter the 2nd number:"))
+    z=div(x,y)
+    print("The division is:",z)
+
+division()    
+          
+def square(p):
+    output=p*p
+    return (output)
+def squ():
+    x=int(input("Enter the  number:"))
+    z=square(x)
+    print("The square is:",z)
+
+squ()    
